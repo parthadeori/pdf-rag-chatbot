79ac6043ed44e05c685c3e3ccdfe818f1b6b9608
Partha <87178424+parthadeori@users.noreply.github.com>
2026-03-15 19:51:43 +0530

    feat: add PDF upload, document loading, text chunking, embeddings, and Chroma vector database setup

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 
 st.title("PDF RAG Chatbot")
 
@@ -23,6 +25,17 @@ if uploaded_file is not None:
     )
 
     chunks = text_splitter.split_documents(documents)
+
+    # Create embeddings
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    # Store embeddings in vector database
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings
+    )
+
+    st.write("Vector database created successfully!")
 
     st.write(f"Total chunks created: {len(chunks)}")
 
